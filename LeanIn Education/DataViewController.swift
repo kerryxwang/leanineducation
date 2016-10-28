8e81a490dd48eebbe12765c967772ce7c6a23967
Shreya Shankar <shreya1@stanford.edu>
2016-10-28 13:00:53 -0700

    Initial Commit

---
+++ LeanIn Education/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  LeanIn Education
+//
+//  Created by Shreya Shankar on 10/28/16.
+//  Copyright © 2016 Shreya Shankar. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
